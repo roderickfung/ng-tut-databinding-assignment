ea1c7f54d53aa0c5c490509787c74e1973738d1c
Roderick Fung <roderickfung@users.noreply.github.com>
2017-08-02 14:13:26 -0700

    add continue counting feature

--- src/app/game-item/game-item.component.ts
+++ src/app/game-item/game-item.component.ts
@@ -10,14 +10,12 @@ export class GameItemComponent implements OnInit {
   @Input('gameItem') public game: { id: number, data: Array<number> };
   public counter: number = 1;
   public count;
+  public counting: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-    this.count = setInterval(() => {
-      this.game.data.push(this.counter)
-      this.counter += 1
-    }, 1000);
+    this._interval();
   }
 
   public isOdd(item) {
@@ -26,6 +24,23 @@ export class GameItemComponent implements OnInit {
 
   public stopInterval() {
     clearInterval(this.count)
+    this.counting = !this.counting;
+  }
+
+  public continueInterval() {
+    this._interval()
+    this.counting = !this.counting;
+  }
+
+  private _interval() {
+    this.count = setInterval(() => {
+      this.game.data.push(this.counter)
+      this.counter += 1
+    }, 1000)
+  }
+
+  public isCounting() {
+    return this.counting;
   }
 
 }
